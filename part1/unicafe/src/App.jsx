c61f88e91b2e6000aa893bbc1daf7c3493c4126b
Rafael Sousa de Morais Junior <contato@rafaelmoraisjr.com>
2025-10-18 14:51:28 -0300

    exercise 1.7 done

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -13,11 +13,17 @@ const Display = (props) => {
 };
 
 const Statistics = (props) => {
+  const all = props.good + props.neutral + props.bad;
+  const average = (props.good - props.bad) / all;
+  const positive = (props.good / all) * 100;
   return (
     <div>
       <p>good: {props.good}</p>
       <p>neutral: {props.neutral}</p>
       <p>bad: {props.bad}</p>
+      <p>all: {all}</p>
+      <p>average: {average}</p>
+      <p>positive: {positive} %</p>
     </div>
   );
 };
